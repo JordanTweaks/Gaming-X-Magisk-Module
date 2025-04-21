831501b06d80d744871bba37c19244d2e4b15f4a
JordanTweaks <uzonijordan12@gmail.com>
2025-04-21 20:01:26 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,94 @@
+##########################################################################################
+#
+# Magisk Module Template Config Script
+# by topjohnwu
+#
+##########################################################################################
+##########################################################################################
+#
+# Instructions:
+#
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (config.sh)
+# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
+# 5. Add your additional or modified system properties into common/system.prop
+#
+##########################################################################################
+
+SKIPMOUNT=false
+
+PROPFILE=true
+
+POSTFSDATA=true
+
+LATESTARTSERVICE=true
+
+REPLACE_EXAMPLE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+
+REPLACE="/vendor/etc/powerhint.xml
+"
+
+print_modname() {
+  ui_print "      Gaming-X Optimizer Version 1.0     "
+busybox sleep 1
+  ui_print "                   "
+busybox sleep 1
+  ui_print " By : JordanTweaks "
+busybox sleep 1
+  ui_print " Installing"
+busybox sleep 5
+  ui_print "                   "
+  busybox sleep 1
+  ui_print "         Please wait..          "
+  ui_print "                   "
+  busybox sleep 1
+  ui_print "                 Loading..."
+  busybox sleep 1
+  ui_print "                 Installing..."
+  ui_print "                 Wait.."
+  busybox sleep 1
+  ui_print " Installing success "
+  busybox sleep 1
+  ui_print " Reboot Your Device "
+  ui_print " _____________________  ______________   __________    ____  __ "
+  ui_print " __  ____/__    |__   |/  /___  _/__  | / /_  ____/    __  |/ / "
+  ui_print " _  / __ __  /| |_  /|_/ / __  / __   |/ /_  / __________    /  "
+  ui_print " / /_/ / _  ___ |  /  / / __/ /  _  /|  / / /_/ //_____/    |   "
+  ui_print " \____/  /_/  |_/_/  /_/  /___/  /_/ |_/  \____/       /_/|_|   "
+}
+
+
+# Copy/extract your module files into $MODPATH in on_install.
+
+on_install() {
+  # The following is the default implementation: extract $ZIPFILE/system to $MODPATH
+  # Extend/change the logic to whatever you want
+  ui_print "- Extracting module files"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+
+# Only some special files require specific permissions
+# This function will be called after on_install is done
+# The default permissions should be good enough for most cases
+
+set_permissions() {
+  # The following is the default rule, DO NOT remove
+  set_perm_recursive $MODPATH 0 0 0755 0644
+  set_perm $MODPATH/system/bin/P0 0 0 0755 0755
+  set_perm $MODPATH/system/bin/P1 0 0 0755 0755
+
+  # Here are some examples:
+  # set_perm_recursive  $MODPATH/system/lib       0     0       0755      0644
+  # set_perm  $MODPATH/system/bin/app_process32   0     2000    0755      u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0     2000    0755      u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0     0       0644
+}
+
+# You can add more functions to assist your custom script code
